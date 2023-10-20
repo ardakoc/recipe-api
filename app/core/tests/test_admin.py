e7aaa9a61a9e77b63462127603aee8179e391ccf
Arda Koç <ardaskoc@gmail.com>
2023-10-20 11:04:54 +0300

    added test method for edit user page works.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,9 +32,25 @@ class AdminSiteTests(TestCase):
         """
         Test users are listed on page.
         """
+        # According to this page:
         # https://docs.djangoproject.com/en/4.2/ref/contrib/admin/#reversing-admin-urls
+        # the layout of the reverse url method of the list (changelist) page must be
+        # {{ app_label }}_{{ model_name }}_changelist:
         url = reverse('admin:core_user_changelist')
         response = self.client.get(url)
 
         self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
+
+    def test_edit_user_page(self):
+        """
+        Test the edit user page works.
+        """
+        # According to this page:
+        # https://docs.djangoproject.com/en/4.2/ref/contrib/admin/#reversing-admin-urls
+        # the layout of the reverse url method of the edit page must be
+        # {{ app_label }}_{{ model_name }}_change and the object_id parameter:
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
