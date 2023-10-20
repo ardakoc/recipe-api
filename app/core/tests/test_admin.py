40e71f9b0b6915be80f0ceaaa016f3bcc0d5117a
Arda Koç <ardaskoc@gmail.com>
2023-10-20 10:46:17 +0300

    added test class for admin site. added test method for list users.

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """
+    Tests for Django admin.
+    """
+
+    def setUp(self):
+        """
+        Create user and client.
+        """
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',
+        )
+
+    def test_users_list(self):
+        """
+        Test users are listed on page.
+        """
+        # https://docs.djangoproject.com/en/4.2/ref/contrib/admin/#reversing-admin-urls
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
