241909c311641ed2eba6801f5e2822f3a794f70a
Arda Koç <ardaskoc@gmail.com>
2023-10-24 16:25:19 +0300

    added test class for unauthenticated api requests. added a test method for this class.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -2,9 +2,19 @@
 Tests for the recipe api.
 """
 from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Recipe
+from recipe.serializers import RecipeSerializer
+
+
+RECIPES_URL = reverse('recipe:recipe-list')
 
 
 def create_recipe(user, **params):
@@ -22,3 +32,20 @@ def create_recipe(user, **params):
 
     recipe = Recipe.objects.create(user=user, **defaults)
     return recipe
+
+
+class PublicRecipeAPITests(TestCase):
+    """
+    Test unauthenticated api requests.
+    """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """
+        Test auth is required to call api.
+        """
+        res = self.client.get(RECIPES_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
