467d359ad830d323603c2d532ca44490eb40161f
Arda Koç <ardaskoc@gmail.com>
2023-10-29 11:06:07 +0300

    implemented the feature that adding new tags when creating recipes.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,26 +6,6 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
-class RecipeSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the recipes.
-    """
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
-
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """
-    Serializer for the recipe detail.
-    """
-
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
-
-
 class TagSerializer(serializers.ModelSerializer):
     """
     Serializer for the tags.
@@ -35,3 +15,38 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
         read_only_fields = ['id']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the recipes.
+    """
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """
+        Create a recipe.
+        """
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        authenticated_user = self.context['request'].user
+        for tag in tags:
+            tag_object, created = Tag.objects.get_or_create(
+                user=authenticated_user, **tag
+            )
+            recipe.tags.add(tag_object)
+        return recipe
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """
+    Serializer for the recipe detail.
+    """
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
