e38d91163533047c0bdf96398f492c7f223077f8
Arda Koç <ardaskoc@gmail.com>
2023-10-31 16:27:51 +0300

    implemented the feature that adding new ingredients when creating recipes.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -33,10 +33,19 @@ class RecipeSerializer(serializers.ModelSerializer):
     Serializer for the recipes.
     """
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+            'tags',
+            'ingredients'
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -48,15 +57,28 @@ class RecipeSerializer(serializers.ModelSerializer):
             tag_object, created = Tag.objects.get_or_create(
                 user=authenticated_user, **tag
             )
-            recipe.tags.add(tag_object)        
+            recipe.tags.add(tag_object)
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """
+        Handle getting or creating ingredients as needed.
+        """
+        authenticated_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_object, created = Ingredient.objects.get_or_create(
+                user=authenticated_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_object)      
 
     def create(self, validated_data):
         """
         Create a recipe.
         """
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
         return recipe
 
     def update(self, instance, validated_data):
