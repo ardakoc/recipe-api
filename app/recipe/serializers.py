5fb81a9fedea606a2966b6de4ee784f965184f80
Arda Koç <ardaskoc@gmail.com>
2023-11-02 17:14:22 +0300

    added image field to the recipe detail serializer.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -107,7 +107,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     """
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
